3667ffd20d7c2d027607c013a6283671b18b879a
Toby16 <bolajibaasit14@gmail.com>
2022-12-21 04:40:04 -0500

    added curly braces to conditionals | corrected return statement

--- _strncmp.c
+++ _strncmp.c
@@ -13,12 +13,23 @@
 int _strncmp(char *str1, char *str2, int n)
 {
 	if (!n)
+	{
 		return (0);
+	}
+
 	if (*str1 == *str2)
+	{
 		return (*str1 ? _strncmp(str1 + 1, str2 + 1, n - 1) : 0);
+	}
+
 	if (*str1)
+	{
 		return (1);
+	}
+
 	if (*str2)
+	{
 		return (-1);
+	}
 	return (*str1 - *str2);
 }
