a883a9ed041a99c364a1023fc098729ebc2054f7
Toby16 <bolajibaasit14@gmail.com>
2022-12-21 04:22:48 -0500

    main

---
+++ main.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+/**
+  * main - runs UNIX command interpreter
+  * Return: 1, on success
+  */
+
+int main(void)
+{
+	int status = 1;
+	char *line;
+	char **args;
+
+	signal(SIGINT, sigint_handler);
+	while (status)
+	{
+		status = isatty(STDIN_FILENO);
+
+		if (status == 1)
+		{
+			write(STDOUT_FILENO, "$ ", 2);
+		}
+
+		line = readline();
+		if (!line)
+		{
+			return (0);
+		}
+
+		if (_strcmp(line, "exit") == 0)
+		{
+			free(line);
+			return (0);
+		}
+
+		if (_strcmp(line, "env") == 0)
+		{
+			_printenv();
+			free(line);
+			continue;
+		}
+
+		args = splitline(line);
+
+		if (args == NULL)
+		{
+			free(line);
+			free(args);
+			continue;
+		}
+
+		status = execute(args);
+		free(line);
+		free(args);
+	}
+	return (0);
+}
